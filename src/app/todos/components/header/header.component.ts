fc4e1e7d76c44f2960b0ba8e5eb8959905dbc495
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-13 22:54:17 +0200

    Implement header section

--- src/app/todos/components/header/header.component.ts
+++ src/app/todos/components/header/header.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TodosService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos-header',
   templateUrl: './header.component.html',
   standalone: true,
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  todosService = inject(TodosService);
+  text: string = '';
+
+  changeText(event: Event): void {
+    const target = event.target as HTMLInputElement;
+    this.text = target.value;
+  }
+
+  addTodo(): void {
+    this.todosService.addTodo(this.text);
+    this.text = '';
+  }
+}
