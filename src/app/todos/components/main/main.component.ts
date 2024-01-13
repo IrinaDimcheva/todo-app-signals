1205182cf3fb3471a032bca2de044dbb4866f526
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-13 23:21:19 +0200

    Implement main section

--- src/app/todos/components/main/main.component.ts
+++ src/app/todos/components/main/main.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodosService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
 })
-export class MainComponent {}
+export class MainComponent {
+  todosService = inject(TodosService);
+}
