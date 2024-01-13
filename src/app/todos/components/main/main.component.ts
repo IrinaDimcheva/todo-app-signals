1bd5d727ae8620c3d50c684cbc1020275ba22301
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-13 21:35:38 +0200

    Split project

---
+++ src/app/todos/components/main/main.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './main.component.html',
+})
+export class MainComponent {}
