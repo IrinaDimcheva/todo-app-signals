c664e31bd78e75f501e798a5decc77bd14a8953b
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-14 02:57:03 +0200

    Implement editing

--- src/app/todos/components/todo/todo.component.ts
+++ src/app/todos/components/todo/todo.component.ts
@@ -1,11 +1,44 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  inject,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TodoInterface } from '../../types/todo.interface';
+import { TodosService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todos-todo',
   templateUrl: './todo.component.html',
   standalone: true,
+  imports: [CommonModule],
 })
-export class TodoComponent {
+export class TodoComponent implements OnInit {
   @Input({ required: true }) todo!: TodoInterface;
+  @Input({ required: true }) isEditing!: boolean;
+  @Output() setEditingId: EventEmitter<string | null> = new EventEmitter();
+
+  todosService = inject(TodosService);
+  editingText: string = '';
+
+  ngOnInit(): void {
+    this.editingText = this.todo.text;
+  }
+
+  changeText(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.editingText = value;
+  }
+
+  changeTodo(): void {
+    this.todosService.changeTodo(this.todo.id, this.editingText);
+    this.setEditingId.emit(null);
+  }
+
+  setTodoInEditMode(): void {
+    this.setEditingId.emit(this.todo.id);
+  }
 }
